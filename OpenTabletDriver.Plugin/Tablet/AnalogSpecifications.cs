27f8a96ee78cbdbc76451705bc21001589c99a97
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2026-01-08 21:53:07 +0100

    Feature: Multi-wheel support

--- OpenTabletDriver.Plugin/Tablet/AnalogSpecifications.cs
+++ OpenTabletDriver.Plugin/Tablet/AnalogSpecifications.cs
@@ -6,20 +6,19 @@ namespace OpenTabletDriver.Plugin.Tablet
     /// <summary>
     /// Device specifications for an analog reporting device, such as knobs, wheels &amp; strips (more commonly touch strips).
     /// </summary>
-    /// <remarks>
-    /// This spec currently does not allow analog devices with different specs.
-    /// </remarks>
     public class AnalogSpecifications
     {
         /// <summary>
-        /// The amount of steps in the analog device.
+        /// The amount of steps in the analog device, minus 1 (as step 0 is a valid step)
+        /// <para/>
+        /// For both relative and absolute wheels, this is the amount of steps per 360 degrees
         /// </summary>
         [Required(ErrorMessage = $"{nameof(StepCount)} must be defined")]
         [JsonProperty(Order = int.MinValue)]
         public uint StepCount { set; get; }
 
         /// <summary>
-        /// Does the device report relative position (movement) or absolute position
+        /// Does the device report relative position (deltas) or absolute position (exact value)
         /// </summary>
         [Required(ErrorMessage = $"{nameof(IsRelative)} must be defined")]
         [JsonProperty(Order = int.MinValue)]
